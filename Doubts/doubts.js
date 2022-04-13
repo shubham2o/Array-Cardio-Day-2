a14cab21c9f0220a1f62149307eabdc017900a0b
shubham2o <shubhamsharma.ha@gmail.com>
2022-04-13 20:33:51 +0530

    Slice() Method added and doubts completed

--- Doubts/doubts.js
+++ Doubts/doubts.js
@@ -20,3 +20,21 @@ document.getElementById("splice5").innerHTML = fruits;
 
 fruits.splice(2, 1, "Lemon", "Kiwi");
 document.getElementById("splice6").innerHTML = fruits;
+
+
+
+// Slice() Method
+const fruitSlice = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+
+// EXAMPLE 1
+document.getElementById("slice1").innerHTML = fruitSlice;
+
+const citrus = fruitSlice.slice(1, 3);
+document.getElementById("slice2").innerHTML = citrus;
+
+
+// EXAMPLE 2
+document.getElementById("slice3").innerHTML = fruitSlice;
+
+const myBest = fruitSlice.slice(-3, -1);
+document.getElementById("slice4").innerHTML = myBest;
